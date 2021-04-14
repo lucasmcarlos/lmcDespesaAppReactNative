25e455f2b3f84117083c224573416738e47886b2
Lucas Maestro - NoteDellInspirion <lucasmcarlos@gmail.com>
2021-04-14 12:04:58 -0300

    trocar link da api

--- src/services/apiDespesas.js
+++ src/services/apiDespesas.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:"http://192.168.0.22:8000/",
+    baseURL:"https://apilmcdespesas-lumen.herokuapp.com/",
     headers:{ },
 });
 
